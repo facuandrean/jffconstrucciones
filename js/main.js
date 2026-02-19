df539af3f499cf636add298a07ac20d43be8d2f1
Facundo Andrean <facundoandrean22@gmail.com>
2026-02-18 21:34:50 -0300

    fix: main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { btnNav } from './btn-nav.js';
 import { slider } from './carousel.js';
-import { formPhp } from './form-php.js';
 import { formFS } from './form-FS.js';
 import { header } from './header.js';
 import { scroll } from './scroll.js';
@@ -12,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     header();
     btnNav();
     slider();
-    // formPhp();
     formFS();
     scroll();
     nav();
